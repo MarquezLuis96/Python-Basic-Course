d53a09c5307320d8f555a5dfb24cb19099d07301
MarquezLuis96 <englamc@gmail.com>
2020-08-30 00:15:43 -0400

    V.0.1.3.1 - Loop While

--- 03 - Loops/loops.py
+++ 03 - Loops/loops.py
@@ -1,3 +1,21 @@
-cont = 0
+#cont = 0
+#
+#print("(2)^(" + str(cont) + ") = " + str(2**cont))
+LIMIT = 1000
 
-print("(2)^(" + str(cont) + ") = " + str(2**cont))
+
+def power_2():
+    cont = 0
+    powr_2 = 2**cont
+    while powr_2 < LIMIT:
+        print("(2)^(" + str(cont) + ") = " + str(powr_2) + "\n")
+        cont = cont + 1
+        powr_2 = cont**2
+
+
+def run():
+    power_2()
+
+
+if __name__ == '__main__':
+    run()
