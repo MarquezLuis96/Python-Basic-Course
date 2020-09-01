c0df966bff8bb113a98cb2b21180a27dda1cdc52
MarquezLuis96 <englamc@gmail.com>
2020-09-01 14:30:09 -0400

    V.0.1.3.3 - Learning how to use loop for

---
+++ 03 - Loops/loop_for.py
@@ -0,0 +1,17 @@
+#Print numbers from 1 to 1000
+
+
+#Solution using while
+#cont = 1
+#while cont < 1000:
+#    print(cont)
+#    cont += 1
+
+#Loop for
+for cont in range(1,1001):
+    print(cont)
+
+
+
+for i in range(11):
+    print(11 * i)
